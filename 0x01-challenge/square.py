341dd784b1415b0c49d3d85f344578f25cd0c19d
Adam <adamelmadani0129@gmail.com>
2023-10-14 00:57:12 +0100

    2.task

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,23 +1,39 @@
 #!/usr/bin/python3
+"""
+This is our module
+"""
 
 
 class square():
-
+    """
+    This is our class
+    """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """
+        This is our constructor
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """
+        Area of the square
+        """
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """
+        Permiter of the square
+        """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """
+        string representation of square
+        """
         return "{}/{}".format(self.width, self.height)
 
 
